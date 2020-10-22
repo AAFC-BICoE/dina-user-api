2867636615364b90b89ff9d18fbe0470b6cc0837
Jonathan Keyuk <Jonkero2@gmail.com>
2020-10-22 11:36:25 -0400

    20645 use dina repo (#3)
    * DinaUserService is a @Service
    * add initial test suite for user repo
    * bump dina base version
    * update repo for not allowed methods and delete some old classes

--- src/main/java/ca/gc/aafc/dinauser/api/dto/DinaUserDto.java
+++ src/main/java/ca/gc/aafc/dinauser/api/dto/DinaUserDto.java
@@ -1,23 +1,37 @@
 package ca.gc.aafc.dinauser.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.crnk.core.resource.annotations.JsonApiId;
+import io.crnk.core.resource.annotations.JsonApiResource;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
+@JsonApiResource(type = "user")
+@Data
+@SuppressFBWarnings({"EI_EXPOSE_REP", "EI_EXPOSE_REP2"})
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DinaUserDto {
-  
+
+  @JsonApiId
   private String internalId;
-  
+
   private String username;
   private String firstName;
   private String lastName;
   private String agentId;
   private String emailAddress;
-  private final List<String> roles = new ArrayList<String>();
-  private final List<String> groups = new ArrayList<String>();
-  
+
+  @JsonInclude(JsonInclude.Include.NON_EMPTY)
+  private final List<String> roles = new ArrayList<>();
+  @JsonInclude(JsonInclude.Include.NON_EMPTY)
+  private final List<String> groups = new ArrayList<>();
+
 }
---
+++ src/main/java/ca/gc/aafc/dinauser/api/repository/UserRepository.java
@@ -0,0 +1,53 @@
+package ca.gc.aafc.dinauser.api.repository;
+
+import ca.gc.aafc.dinauser.api.dto.DinaUserDto;
+import ca.gc.aafc.dinauser.api.service.DinaUserService;
+import io.crnk.core.exception.MethodNotAllowedException;
+import io.crnk.core.queryspec.QuerySpec;
+import io.crnk.core.repository.ResourceRepositoryBase;
+import io.crnk.core.resource.list.ResourceList;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+import java.util.Collection;
+
+@Repository
+public class UserRepository extends ResourceRepositoryBase<DinaUserDto, String> {
+
+  @Inject
+  private DinaUserService service;
+
+  public UserRepository() {
+    super(DinaUserDto.class);
+  }
+
+  @Override
+  public ResourceList<DinaUserDto> findAll(QuerySpec querySpec) {
+    return this.findAll(null, querySpec);
+  }
+
+  @Override
+  public DinaUserDto findOne(String id, QuerySpec querySpec) {
+    return service.getUser(id);
+  }
+
+  @Override
+  public ResourceList<DinaUserDto> findAll(Collection<String> ids, QuerySpec querySpec) {
+    return querySpec.apply(service.getUsers());
+  }
+
+  @Override
+  public <S extends DinaUserDto> S save(S resource) {
+    throw new MethodNotAllowedException("patch is currently not supported");
+  }
+
+  @Override
+  public <S extends DinaUserDto> S create(S resource) {
+    throw new MethodNotAllowedException("post is currently not supported");
+  }
+
+  @Override
+  public void delete(String id) {
+    service.deleteUser(id);
+  }
+}
