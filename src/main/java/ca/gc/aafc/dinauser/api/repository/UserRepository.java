0b1cca93d2271c738ebfd875e93418c842889871
Jkeyuk <Jonkero2@gmail.com>
2020-11-02 08:54:30 -0500

    20663 enable authorization
    Staff and student roles can only return there own result for find all

--- src/main/java/ca/gc/aafc/dinauser/api/repository/UserRepository.java
+++ src/main/java/ca/gc/aafc/dinauser/api/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package ca.gc.aafc.dinauser.api.repository;
 
+import ca.gc.aafc.dina.security.DinaAuthenticatedUser;
+import ca.gc.aafc.dina.security.DinaRole;
 import ca.gc.aafc.dinauser.api.dto.DinaUserDto;
 import ca.gc.aafc.dinauser.api.service.DinaUserService;
 import io.crnk.core.exception.MethodNotAllowedException;
+import io.crnk.core.queryspec.FilterOperator;
+import io.crnk.core.queryspec.PathSpec;
 import io.crnk.core.queryspec.QuerySpec;
 import io.crnk.core.repository.ResourceRepositoryBase;
 import io.crnk.core.resource.list.ResourceList;
@@ -16,13 +20,19 @@ public class UserRepository extends ResourceRepositoryBase<DinaUserDto, String> 
 
   @Inject
   private DinaUserService service;
+  private final DinaAuthenticatedUser authenticatedUser;
 
-  public UserRepository() {
+  public UserRepository(DinaAuthenticatedUser authenticatedUser) {
     super(DinaUserDto.class);
+    this.authenticatedUser = authenticatedUser;
   }
 
   @Override
   public ResourceList<DinaUserDto> findAll(QuerySpec querySpec) {
+    if (!isCollectionManager(authenticatedUser)) {
+      querySpec.addFilter(
+        PathSpec.of("agentId").filter(FilterOperator.EQ, authenticatedUser.getAgentIdentifer()));
+    }
     return this.findAll(null, querySpec);
   }
 
@@ -49,5 +59,11 @@ public class UserRepository extends ResourceRepositoryBase<DinaUserDto, String> 
   @Override
   public void delete(String id) {
     service.deleteUser(id);
+  }
+
+  private static boolean isCollectionManager(DinaAuthenticatedUser authenticatedUser) {
+    return authenticatedUser.getRolesPerGroup().values().stream()
+      .anyMatch(dinaRoles -> dinaRoles.stream()
+        .anyMatch(dinaRole -> dinaRole.equals(DinaRole.COLLECTION_MANAGER)));
   }
 }
