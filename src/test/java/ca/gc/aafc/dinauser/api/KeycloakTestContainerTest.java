2b711496a3ccfab772c7ce17c0fe438aef82a4bf
xuemeigan <xuemei.gan@canada.ca>
2020-11-03 09:18:13 -0500

    Support 20690 Add keycloak TestContainer to allow integration test - Add pom dependency for keycloak test container - Add test to keycloak test container

---
+++ src/test/java/ca/gc/aafc/dinauser/api/KeycloakTestContainerTest.java
@@ -0,0 +1,44 @@
+package ca.gc.aafc.dinauser.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.testcontainers.junit.jupiter.Container;
+
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+
+public class KeycloakTestContainerTest {
+
+    @Container
+    private KeycloakContainer keycloak = new KeycloakContainer("jboss/keycloak:11.0.2")
+    .withRealmImportFile("keycloak-dina-starter-realm.json")
+    .withAdminUsername("admin")  
+    .withAdminPassword("admin")
+    .withExposedPorts(80);
+
+    @BeforeEach
+    public void before(){
+        keycloak.start();
+    }
+     
+    @AfterEach
+    public void teardown(){
+        keycloak.close();
+    }
+
+    @Test
+    public void testContainerStarts(){
+       assertNotNull(keycloak);
+       assertTrue(keycloak.isRunning());
+       assertNotNull(keycloak.getContainerId());
+       assertEquals( "jboss/keycloak:11.0.2", keycloak.getDockerImageName());
+       assertEquals("admin" , keycloak.getAdminUserName());
+       assertEquals("password" , keycloak.getAdminPassword());
+    }
+
+}
