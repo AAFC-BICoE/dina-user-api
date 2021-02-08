5810f73f21f355cd3b2c0f1b0d78dafd1b2fbc55
Jkeyuk <jonkero2@gmail.com>
2021-02-08 12:01:45 -0500

    21601 user preference
    adjust database migration for initial db setup with test

---
+++ src/test/java/ca/gc/aafc/dinauser/api/AppStartsIT.java
@@ -0,0 +1,19 @@
+package ca.gc.aafc.dinauser.api;
+
+import ca.gc.aafc.dina.testsupport.PostgresTestContainerInitializer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest(classes = DinaUserModuleApiLauncher.class)
+@TestPropertySource(properties = "spring.config.additional-location=classpath:application-test.yml")
+@ContextConfiguration(initializers = {PostgresTestContainerInitializer.class})
+public class AppStartsIT {
+
+  @Test
+  void appStarts() {
+
+  }
+
+}
