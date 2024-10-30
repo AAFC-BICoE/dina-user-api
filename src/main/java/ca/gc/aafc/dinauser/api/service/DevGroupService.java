92189b7aecaf4c69d01eec14632a946740af8b4b
Christian Gendreau <christiangendreau@gmail.com>
2024-10-30 11:55:54 -0400

    Implemented Dev based responses

--- src/main/java/ca/gc/aafc/dinauser/api/service/DevGroupService.java
+++ src/main/java/ca/gc/aafc/dinauser/api/service/DevGroupService.java
@@ -1,13 +1,16 @@
 package ca.gc.aafc.dinauser.api.service;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
+import java.util.UUID;
 import lombok.extern.log4j.Log4j2;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
-import ca.gc.aafc.dina.config.DevSettings;
+import ca.gc.aafc.dina.security.DevUserConfig;
 import ca.gc.aafc.dinauser.api.dto.DinaGroupDto;
 import ca.gc.aafc.dinauser.api.dto.DinaGroupMembershipDto;
 
@@ -16,28 +19,33 @@ import ca.gc.aafc.dinauser.api.dto.DinaGroupMembershipDto;
 @Log4j2
 public class DevGroupService implements DinaGroupService {
 
-  private final DevSettings devSettings;
+  private final Map<String, DinaGroupDto> uuidToGroup;
 
-  public DevGroupService(DevSettings devSettings) {
-    this.devSettings = devSettings;
+  public DevGroupService(DevUserConfig devUserConfig) {
+    
+    // generate UUIDs for all groups
+    Map<String, DinaGroupDto> uuidGroupBuilder = new HashMap<>();
+    for (String group : devUserConfig.getGroups()) {
+      String uuid = UUID.randomUUID().toString();
+      uuidGroupBuilder.put(uuid, DinaGroupDto.builder().internalId(uuid).name(group).build());
+    }
+    uuidToGroup = Collections.unmodifiableMap(uuidGroupBuilder);
   }
 
   @Override
   public List<DinaGroupDto> getGroups() {
-    return devSettings.getRolesPerGroup().keySet()
-      .stream()
-      .map(k -> DinaGroupDto.builder().name(k).build())
-      .collect(Collectors.toList());
+    return List.copyOf(uuidToGroup.values());
   }
 
   @Override
   public List<DinaGroupDto> getGroups(Integer firstResult, Integer maxResults) {
-    return List.of();
+    // not enough groups in dev to support that
+    return getGroups();
   }
 
   @Override
   public DinaGroupDto getGroup(String id) {
-    return null;
+    return uuidToGroup.get(id);
   }
 
   @Override
@@ -47,6 +55,10 @@ public class DevGroupService implements DinaGroupService {
 
   @Override
   public DinaGroupMembershipDto getGroupMembership(String identifier) {
+
+    if(uuidToGroup.containsKey(identifier)) {
+      return DinaGroupMembershipDto.builder().name(identifier).build();
+    }
     return null;
   }
 }
--- src/main/java/ca/gc/aafc/dinauser/api/service/DevUserService.java
+++ src/main/java/ca/gc/aafc/dinauser/api/service/DevUserService.java
@@ -1,13 +1,19 @@
 package ca.gc.aafc.dinauser.api.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.extern.log4j.Log4j2;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
-import ca.gc.aafc.dina.config.DevSettings;
+import ca.gc.aafc.dina.security.DevUserConfig;
+import ca.gc.aafc.dina.security.DinaRole;
 import ca.gc.aafc.dinauser.api.dto.DinaUserDto;
 
 @Service
@@ -15,29 +21,47 @@ import ca.gc.aafc.dinauser.api.dto.DinaUserDto;
 @Log4j2
 public class DevUserService implements DinaUserService {
 
-  private final DevSettings devSettings;
+  private final DinaUserDto devUserDto;
 
-  public DevUserService(DevSettings devSettings) {
-    this.devSettings = devSettings;
+  public DevUserService(DevUserConfig devUserConfig) {
+    // map DineRole to its string value
+    Map<String, Set<String>> rolesPerGroup = new HashMap<>();
+    for (var entry : devUserConfig.getRolesPerGroup().entrySet()) {
+      rolesPerGroup.put(entry.getKey(),
+        entry.getValue().stream().map(DinaRole::getKeycloakRoleName).collect(
+          Collectors.toSet()));
+    }
+
+    devUserDto = DinaUserDto.builder()
+      .username(devUserConfig.getUsername())
+      .internalId(devUserConfig.getInternalId())
+      .rolesPerGroup(rolesPerGroup)
+      .build();
   }
 
   @Override
   public List<DinaUserDto> getAllUsers() {
-    return List.of();
+    return List.of(devUserDto);
   }
 
   @Override
   public List<DinaUserDto> getUsers(Integer firstResult, Integer maxResults) {
-    return List.of();
+    return getAllUsers();
   }
 
   @Override
   public List<DinaUserDto> getUsers(Set<String> groups) {
+    if (CollectionUtils.isProperSubCollection(groups, devUserDto.getRolesPerGroup().keySet())) {
+      return List.of(devUserDto);
+    }
     return List.of();
   }
 
   @Override
   public DinaUserDto findOne(Object naturalId) {
+    if (devUserDto.getInternalId().equals(Objects.toString(naturalId))) {
+      return devUserDto;
+    }
     return null;
   }
 
@@ -48,7 +72,7 @@ public class DevUserService implements DinaUserService {
 
   @Override
   public void deleteUser(String id) {
-
+    // no-op
   }
 
   @Override
