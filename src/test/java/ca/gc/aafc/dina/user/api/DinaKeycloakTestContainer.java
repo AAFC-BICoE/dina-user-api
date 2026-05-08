16371fda25a088440bb3d20fdf763714b74d2aa2
Christian Gendreau <christiangendreau@gmail.com>
2026-05-08 09:29:30 -0400

    Updated starter realm file

--- src/test/java/ca/gc/aafc/dina/user/api/DinaKeycloakTestContainer.java
+++ src/test/java/ca/gc/aafc/dina/user/api/DinaKeycloakTestContainer.java
@@ -3,34 +3,38 @@ package ca.gc.aafc.dina.user.api;
 import dasniko.testcontainers.keycloak.KeycloakContainer;
 
 public class DinaKeycloakTestContainer extends KeycloakContainer {
-    private static final String IMAGE_VERSION = UserModuleTestKeycloakConfiguration.KEYCLOAK_DOCKER_IMAGE;
-    private static final String REALM_FILE_NAME = "keycloak-dina-starter-realm.json";
-    private static final String ADMIN_USER_NAME = "admin";
-    private static final String ADMIN_PASS_WORD = "admin";
-    private static DinaKeycloakTestContainer container;
- 
-    private DinaKeycloakTestContainer() {
-        super(IMAGE_VERSION);
+  private static final String IMAGE_VERSION =
+    UserModuleTestKeycloakConfiguration.KEYCLOAK_DOCKER_IMAGE;
+  
+  private static final String REALM_FILE_NAME = "keycloak-dina-starter-realm.json";
+  private static final String ADMIN_USER_NAME = "admin";
+  private static final String ADMIN_PASS_WORD = "admin";
+  private static DinaKeycloakTestContainer container;
+
+  private DinaKeycloakTestContainer() {
+    super(IMAGE_VERSION);
+  }
+
+  public static DinaKeycloakTestContainer getInstance() {
+    if (container == null) {
+      container = new DinaKeycloakTestContainer();
     }
- 
-    public static DinaKeycloakTestContainer getInstance() {
-        if (container == null) {
-            container = new DinaKeycloakTestContainer();
-        }
-        return container;
-    }
- 
-    @Override
-    public void start() {
-        container.withRealmImportFile(REALM_FILE_NAME)
-        .withAdminUsername(ADMIN_USER_NAME)  
-        .withAdminPassword(ADMIN_PASS_WORD);
-        super.start();
-    }
- 
-    @Override
-    public void stop() {
-        //do nothing, JVM handles shut down
-    }    
-    
+    return container;
+  }
+
+  @Override
+  public void start() {
+    container.withRealmImportFile(REALM_FILE_NAME)
+      .withAdminUsername(ADMIN_USER_NAME)
+      .withAdminPassword(ADMIN_PASS_WORD);
+
+    // Enable that line to see the output of the continaer. Usefull on version
+    //.withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("keycloak")));
+    super.start();
+  }
+
+  @Override
+  public void stop() {
+    //do nothing, JVM handles shut down
+  }
 }
